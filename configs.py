d946dcff1beac1ac06f1bc1b9e75eab0ac03c7ca
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-07-24 08:04:55 -0700

    Fix pylint

--- configs.py
+++ configs.py
@@ -1,4 +1,17 @@
-LOGIN_URL = "https://sso.hcmut.edu.vn/cas/login?service=https%3A%2F%2Fe-learning.hcmut.edu.vn%2Flogin%2Findex.php%3FauthCAS%3DCAS"
+#!/usr/bin/env python3
+# Copyright (c) Stanford University and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+This module contains configurations for crawling pipeline.
+"""
+
+LOGIN_URL = (
+    "https://sso.hcmut.edu.vn/cas/login?service="
+    "https%3A%2F%2Fe-learning.hcmut.edu.vn%2Flogin%2Findex.php%3FauthCAS%3DCAS"
+)
 LOGIN_USER = "010344"
 LOGIN_PASSWD = "010344"
 
