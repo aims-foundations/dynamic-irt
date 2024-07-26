648914b54503c95bf2cfaaab24a00c488a6e61ae
Thu Nguyen <painkiller.nhat@gmail.com>
2024-07-26 21:11:14 +0700

    Fix main_crawling.py

--- configs.py
+++ configs.py
@@ -1,4 +1,3 @@
-LOGIN_URL = "https://sso.hcmut.edu.vn/cas/login?service=https%3A%2F%2Fe-learning.hcmut.edu.vn%2Flogin%2Findex.php%3FauthCAS%3DCAS"
 LOGIN_USER = "010344"
 LOGIN_PASSWD = "010344"
 
@@ -6,5 +5,21 @@ DATA_LINKS = {
     "DSA-HK231": {
         "L09": "https://e-learning.hcmut.edu.vn/course/view.php?id=106649",
         "DT01": "https://e-learning.hcmut.edu.vn/course/view.php?id=108885",
+        "CN01": "https://e-learning.hcmut.edu.vn/course/view.php?id=109196",
+        "CN02": "https://e-learning.hcmut.edu.vn/course/view.php?id=109195",
+        "ALL": "https://e-learning.hcmut.edu.vn/course/view.php?id=116047",
+    },
+    "DSA-HK221": {
+        "ALL": "https://e-learning.hcmut.edu.vn/course/view.php?id=74329",
+    },
+    "PF-HK232": {
+        "ALL": "https://lms.hcmut.edu.vn/course/view.php?id=31275",
+        "ALL-1027": "https://lms.hcmut.edu.vn/course/view.php?id=31071",
+    },
+    "PF-HK222": {
+        "ALL": "https://e-learning.hcmut.edu.vn/course/view.php?id=94316",
     }
+    
 }
+
+# QUIZ_PREFIX_LINK = "https://{SITE}/mod/quiz/view.php?id="
