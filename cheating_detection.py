1e66d2d4599f35c67e8497d393df6ba2f3e89992
Thu Nguyen <painkiller.nhat@gmail.com>
2024-09-10 10:26:35 -0700

    add tqdm and copying student records

--- cheating_detection.py
+++ cheating_detection.py
@@ -3,15 +3,13 @@ import os
 import pickle
 import shutil
 from urllib.parse import unquote, urlsplit
-
+from tqdm import tqdm
 import Levenshtein
 import numpy as np
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-from datasets import config, load_dataset
-from huggingface_hub import HfApi
+from huggingface_hub import snapshot_download
 import datetime
+from collections import defaultdict
 
 
 def find_json_files(repo_id):
@@ -51,18 +49,13 @@ def find_json_files(repo_id):
     return directory_json_files
 
 
-def fetch_unique_questions(url):
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        try:
-            # print(response.content)
-            unique_questions = pickle.loads(response.content)
-            return unique_questions
-        except Exception as e:
-            print(f"Failed to load and deserialize the pickle file: {e}")
-            return []
-    else:
-        print(f"Failed to download data: HTTP {response.status_code}")
+def fetch_unique_questions(filepath):
+    try:
+        with open(filepath, 'rb') as file:  # Open the file in binary read mode
+            unique_questions = pickle.load(file)
+        return unique_questions
+    except Exception as e:
+        print(f"Failed to load and deserialize the pickle file: {e}")
         return []
 
 def preprocess_question(question):
@@ -99,23 +92,25 @@ def similarity(s1, s2):
 
 def filter_cheating_between_students(student_records, threshold=0.8):
     if not student_records:
-        return []
+        return [], []
 
     filtered_records = [tokenize(student_records.pop(0))]
+    potential_copies = []
 
     while student_records:
         current_record = tokenize(student_records.pop(0))
         is_unique = True
 
         for accepted_record in filtered_records:
-            if edit_distance(current_record, accepted_record) > threshold:
+            if edit_distance(current_record, accepted_record) <= threshold:
                 is_unique = False
+                potential_copies.append(current_record)  # Store potential copies
                 break
 
         if is_unique:
             filtered_records.append(current_record)
 
-    return filtered_records
+    return filtered_records, potential_copies
 
 def detect_cheating(records, change_threshold=0.9, time_threshold=datetime.timedelta(minutes=10)):
     if not records:
@@ -152,80 +147,88 @@ def find_matching_question(question_text, unique_questions, threshold=0.8):
     
     return None
 
-def main(repo_id, directory_json_files, threshold=0.9):
-    unique_questions = fetch_unique_questions(f"https://huggingface.co/datasets/{repo_id}/resolve/main/unique_questions.pkl")
+def main(repo_id, threshold=0.9):
+    unique_questions = fetch_unique_questions(f"{repo_id}/unique_questions.pkl")
     all_records = {}
     cheaters = {}
+    copying_records = {}
 
-    for directory, files in directory_json_files.items():
-        for file in files:
-            data_q = load_dataset(
-                repo_id, data_files=f"{directory}/{file}", field="list_questions"
-            )
-            list_questions = [q["question"] for q in data_q["train"]]
-                
-            data_s = load_dataset(
-                repo_id, data_files=f"{directory}/{file}", field="student_answers"
-            )
-            base_name, _ = os.path.splitext(file)
+    for root, dirs, files in os.walk(repo_id):
+        for file in tqdm(files, desc="Processing files"):
+            if file.endswith('.json'):
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, 'r') as json_file:
+                        data = json.load(json_file)
+                        lab_name = data['lab_name']
+                        list_questions = [q["question"] for q in data["list_questions"]]
+                        
+                        for answer in tqdm(data["student_answers"], desc="Processing answers", leave=False):
+                            student_id = answer["id"]
+                            if student_id in cheaters:
+                                continue
 
-            for answer in data_s["train"]:
-                student_id = answer["id"]
-                
-                if student_id in cheaters:
-                    continue
+                            for q_idx, response in enumerate(answer["response_history"]):
+                                matching_question = find_matching_question(list_questions[q_idx], unique_questions)
 
-                for q_idx, response in enumerate(answer["response_history"]):
-                    matching_question = find_matching_question(list_questions[q_idx], unique_questions)
+                                if not matching_question:
+                                    print(lab_name, file_path, list_questions[q_idx])
+                                    matching_question = "Unmatched Questions"
+                                
+                                # question_id = response["question"]
+                                student_responses = []
+                                last_response = None
+                                last_correct_response = None
+                                
+                                for r_text in response["results"]:
+                                    if r_text["state"] not in ["Not complete", "Precheck results", "Incomplete answer"] and "Attempt finished" not in r_text["action"]:
+                                        timestamp = datetime.datetime.strptime(r_text["time"], "%d/%m/%y, %H:%M:%S")
+                                        student_responses.append((r_text["action"], timestamp, r_text["state"]))
 
-                    if not matching_question:
-                        print(base_name, file_path, list_questions[q_idx])
-                        matching_question = "Unmatched Questions"
-                    
-                    # question_id = response["question"]
-                    student_responses = []
-                    last_response = None
-                    last_correct_response = None
-                    
-                    for r_text in response["results"]:
-                        if r_text["state"] not in ["Not complete", "Precheck results", "Incomplete answer"] and "Attempt finished" not in r_text["action"]:
-                            timestamp = datetime.datetime.strptime(r_text["time"], "%d/%m/%y, %H:%M:%S")
-                            student_responses.append((r_text["action"], timestamp, r_text["state"]))
+                                        last_response = {
+                                            "response": r_text["action"],
+                                            "file_path": file_path
+                                        }
+                                        if r_text["state"] == "Correct":
+                                            last_correct_response = last_response
 
-                            last_response = {
-                                "response": r_text["action"],
-                                "file_path": file
-                            }
-                            if r_text["state"] == "Correct":
-                                last_correct_response = last_response
+                                # print(student_responses)
+                                cheating_details = detect_cheating(student_responses)
+                                if cheating_details != []:
+                                    cheaters.setdefault(student_id, []).append({
+                                        "lab": lab_name,
+                                        "file": file_path,
+                                        "question_id": response["question"],
+                                        "cheating_details": cheating_details
+                                    })
 
-                    # print(student_responses)
-                    cheating_details = detect_cheating(student_responses)
-                    if cheating_details != []:
-                        cheaters.setdefault(student_id, []).append({
-                            "lab": base_name,
-                            "file": file,
-                            "question_id": response["question"],
-                            "cheating_details": cheating_details
-                        })
-                        break
-
-                    else:
-                        final_response = last_correct_response if last_correct_response else last_response
-                        if final_response:
-                            all_records.setdefault(matching_question, {}).setdefault(student_id, []).append(final_response)
+                                else:
+                                    final_response = last_correct_response if last_correct_response else last_response
+                                    if final_response:
+                                        all_records.setdefault(matching_question, {}).setdefault(student_id, []).append(final_response)
+                        
+                except json.JSONDecodeError as e:
+                    print(f"Error decoding JSON from file: {file_path}: {e}")
 
     filtered_all_records = {}
-    for q, s_responses in all_records.items():
-        filtered_all_records[q] = {}
-        for sid, s_response in s_responses.items():
-            responses_list = [resp["response"] for resp in s_response]
-            filtered_responses = filter_cheating_between_students(responses_list)
+    for question, responses_by_students in tqdm(all_records.items(), desc="Filtering records"):
+        filtered_all_records[question] = {}
+        for student_id, responses in responses_by_students.items():
+            response_texts = [resp["response"] for resp in responses]
+            if len(response_texts) > 1:
+                filtered_responses, copies = filter_cheating_between_students(response_texts)
+                filtered_all_records[question][student_id] = [{"response": resp} for resp in filtered_responses]
+                if copies:
+                    copying_records[question] = copies
+            else:
+                filtered_all_records[question][student_id] = responses
 
-            filtered_all_records[q][sid] = [{"response": resp} for resp in filtered_responses]
 
     with open('cheating_records.json', 'w') as f:
         json.dump(cheaters, f, indent=4, default=datetime_converter)
+
+    with open('copying_records.json', 'w') as f:
+        json.dump(copying_records, f, indent=4)
 
     with open('non_cheating_records.json', 'w') as f:
         json.dump(filtered_all_records, f, indent=4, default=datetime_converter)
@@ -234,12 +237,8 @@ def main(repo_id, directory_json_files, threshold=0.9):
 
 if __name__ == "__main__":
     repo_id = "stair-lab/dsa_hk231_records"
-
-    directory_json_files = find_json_files(repo_id)
-    main(
-        repo_id, directory_json_files
+    data_folder = snapshot_download(
+        repo_id=repo_id, repo_type="dataset"
     )
 
-    cache_dir = config.HF_DATASETS_CACHE
-    shutil.rmtree(cache_dir)
-    os.makedirs(cache_dir, exist_ok=True)
+    main(data_folder)
