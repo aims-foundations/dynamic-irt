2871595b3ebe6b059911239cfff6b83b26c7c9de
Thu Nguyen <painkiller.nhat@gmail.com>
2024-09-16 09:52:30 -0700

    rewrite analysis code

--- utils.py
+++ utils.py
@@ -3,10 +3,6 @@ import os
 import re
 from urllib.parse import unquote, urlsplit
 
-import requests
-from bs4 import BeautifulSoup
-from datasets import load_dataset
-
 
 def parse_score(header_text):
     search_result = re.search(r"/(\d+\.\d+)", header_text)
@@ -37,63 +33,51 @@ def filter_class_group(details, sid, class_name):
 def find_global_max(repo_id, course_name, class_name):
     global_max = 0
 
-    url = f"https://huggingface.co/datasets/{repo_id}/tree/main/{class_name}/"
-    response = requests.get(url)
-    json_files = []
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
-        links = soup.find_all("a")
+    for root, dirs, files in os.walk(repo_id):
+        if class_name in dirs:
+            class_dir_path = os.path.join(root, class_name)
+            
+            for subdir_root, subdir_dirs, subdir_files in os.walk(class_dir_path):
+                for file in subdir_files:
+                    if file.endswith(".json"):
+                        file_path = os.path.join(subdir_root, file)
+                        try:
+                            with open(file_path, "r") as json_file:
+                                data = json.load(json_file)
+                                ids = []
+                                for answers in data["student_answers"]:
+                                    ids.append(answers["id"])
+                        
+                                for idx in range(len(data["list_questions"])):
+                                    max_score = data["list_questions"][idx]["max_scores"]
+                                    q_index = idx + 1
+                        
+                                    records = []
+                                    for answers in data["student_answers"]:
+                                        for answer in answers["response_history"]:
+                                            marks = []
+                                            if answer["question"] == f"Question {q_index}":
+                                                mark_per_attempt = []
+                                                for score_idx in range(len(answer["results"])):
+                                                    if answer["results"][score_idx]["marks"] != "":
+                                                        mark_per_attempt.append(
+                                                            answer["results"][score_idx]["marks"]
+                                                        )
+                        
+                                            marks.append(mark_per_attempt)
+                                        records.extend(marks)
+                        
+                                    try:
+                                        records = [
+                                            [float(mark) * 10 / max_score for mark in student_marks]
+                                            for student_marks in records
+                                        ]
+                                    except:
+                                        continue
+                        
+                                    max_attempts = [len(student_marks) for student_marks in records]
+                                    global_max = max(global_max, max(max_attempts))
 
-        for link in links:
-            href = link.get("href")
-            if href.endswith(".json"):
-                path = urlsplit(href).path
-                filename = path.split("/")[-1]
-                filename = unquote(filename)
-                json_files.append(filename)
-    else:
-        print("Failed to retrieve data:", response.status_code)
-
-    for json_file in json_files:
-        data_q = load_dataset(
-            repo_id, data_files=f"{class_name}/{json_file}", field="list_questions"
-        )
-        data_s = load_dataset(
-            repo_id, data_files=f"{class_name}/{json_file}", field="student_answers"
-        )
-
-        ids = []
-        for answers in data_s["train"]:
-            ids.append(answers["id"])
-
-        for idx in range(len(data_q["train"])):
-            max_score = data_q["train"][idx]["max_scores"]
-            q_index = idx + 1
-
-            records = []
-            for answers in data_s["train"]:
-                for answer in answers["response_history"]:
-                    marks = []
-                    if answer["question"] == f"Question {q_index}":
-                        mark_per_attempt = []
-                        for score_idx in range(len(answer["results"])):
-                            if answer["results"][score_idx]["marks"] != "":
-                                mark_per_attempt.append(
-                                    answer["results"][score_idx]["marks"]
-                                )
-
-                    marks.append(mark_per_attempt)
-                records.extend(marks)
-
-            try:
-                records = [
-                    [float(mark) * 10 / max_score for mark in student_marks]
-                    for student_marks in records
-                ]
-            except:
-                continue
-
-            max_attempts = [len(student_marks) for student_marks in records]
-            global_max = max(global_max, max(max_attempts))
-
+                        except json.JSONDecodeError as e:
+                            print(f"Error decoding JSON from file: {file_path}: {e}")
     return global_max
